781de9b9d78f99c03bd710307c5310e7eb4e373e
Jared Sackett <jake850@gmail.com>
2018-10-03 09:10:11 -0500

    Updated stuff

--- Script.js
+++ Script.js
@@ -1,38 +1,38 @@
-var count = 0;
 
-const resetVars = () => {
-  count = 0;
-};
-
-const searchAndDestroy = (searchTerm, data) => {
-  // data should be in the form of an array/list/json variable
-
-  for (var a in data) {
-    for (var b in data[a]) {
-      if (data[a][b].toUpperCase().replace(/\s/g, '') == searchTerm.toUpperCase().replace(/\s/g, '')) {
-        console.info("FOUND A MATCH for " + searchTerm);
-        count++;
-      }
-    }
-  };
-};
 
 var app = new Vue({
   el: '#app',
-  data: function () {
-    return {
-      file1Contents: [],
-      file2Contents: [],
-      numOfDeletions: 0,
-      outputFileContents: []
-    }
+  data: {
+    file1Contents: [],
+    file2Contents: [],
+    numScanned: 0,
+    numOfDeletions: 0,
+    outputFileContents: [],
+    processingStatus: 0
   },
   methods: {
+    resetVars() {
+      this.numScanned = 0;
+      this.numOfDeletions = 0;
+      this.outputFileContents = [];
+      this.processingStatus = 0;
+    },
+
+    searchAndDestroy(searchTerm, data) {
+      for (var a in data) {
+        for (var b in data[a]) {
+          if (data[a][b].toUpperCase().replace(/\s/g, '') == searchTerm.toUpperCase().replace(/\s/g, '')) {
+            // console.info("FOUND A MATCH for " + searchTerm);
+            this.$data.numOfDeletions++;
+          }
+          this.$data.numScanned++;
+        }
+      };
+    },
 
     // Methods to load files and parse them into JSON data
     loadFile1(ev) {
-      resetVars();
-      this.numOfDeletions = count;
+      this.resetVars();
       const file1 = ev.target.files[0];
       Papa.parse(file1, {
         skipEmptyLines: true,
@@ -42,10 +42,8 @@ var app = new Vue({
         }
       });
     },
-
     loadFile2(ev) {
-      resetVars();
-      this.numOfDeletions = count;
+      this.resetVars();
       const file2 = ev.target.files[0];
       Papa.parse(file2, {
         skipEmptyLines: true,
@@ -57,10 +55,15 @@ var app = new Vue({
     },
 
     processData(ev) {
-      for (var c in this.file2Contents){
-        searchAndDestroy(this.file2Contents[c][0], this.file1Contents);
-        this.numOfDeletions = count;
-      }
+      console.log("Beginning processing data");
+      this.$data.processingStatus = 1;
+      setTimeout(() => {
+        for (var c in this.file2Contents){
+          this.searchAndDestroy(this.file2Contents[c][0], this.file1Contents);
+        }
+        this.$data.processingStatus = 2;
+        console.log("Finished processing data");
+      }, 10);
     }
   }
 });
